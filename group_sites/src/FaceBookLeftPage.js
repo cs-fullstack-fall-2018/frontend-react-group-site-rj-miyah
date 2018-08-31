b42f14b69b6466a12433a61cdbd4a634c89b89eb
Myiah Moore <myiahsade@gmail.com>
2018-08-31 15:18:04 -0500

    check in

--- group_sites/src/FaceBookLeftPage.js
+++ group_sites/src/FaceBookLeftPage.js
@@ -14,7 +14,10 @@ class FaceBookLeftPage extends Component {
                         src="https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-xap1/t39.2365-6/851565_602269956474188_918638970_n.png"
                         alt="" className="iconwrap fb1"/>
                     <div className="fb1">
-                        <span className="rowtext"></span>
+                        <span className="rowtext"> </span>
+                        <h2>Connect with your friends and the</h2>
+                        <h2>world around you on facebook.</h2>
+                        <p>
                         <span className="rowtext2 fb1"></span>
                     </div>
                 </div>
